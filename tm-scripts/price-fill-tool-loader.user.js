1577e54bcfe9c0a5bf5de8435f412919bc131a65
pandansu <dank15x@gmail.com>
2026-08-15 13:38:11 +0800

    Add files via upload

---
+++ tm-scripts/price-fill-tool-loader.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Price Fill Tool (Remote Loader)
+// @namespace    http://tampermonkey.net/
+// @version      3.3
+// @description  Google Sheet or Excel price fill using EAN 商品编码
+// @match        https://scsm-djifx.lingyingdms.com/*
+// @exclude      https://scsm-djifx.lingyingdms.com/imeiprint*
+// @require      https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js
+// @grant        GM_xmlhttpRequest
+// @connect      docs.google.com
+// @connect      googleusercontent.com
+// @connect      docs.googleusercontent.com
+// @require      https://raw.githubusercontent.com/pandansu/eanloc/refs/heads/main/tm-scripts/quantity-checker-core.js?v=2.5
+// @updateURL    https://raw.githubusercontent.com/pandansu/eanloc/refs/heads/main/tm-scripts/quantity-checker-loader.user.js
+// @downloadURL  https://raw.githubusercontent.com/pandansu/eanloc/refs/heads/main/tm-scripts/quantity-checker-loader.user.js
+// ==/UserScript==
