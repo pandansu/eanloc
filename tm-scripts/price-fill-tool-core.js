530de67583e9aaf2e77d74cb0d01e0706c85129b
pandansu <dank15x@gmail.com>
2026-08-15 14:47:00 +0800

    Update price-fill-tool-core.js

--- tm-scripts/price-fill-tool-core.js
+++ tm-scripts/price-fill-tool-core.js
@@ -54,7 +54,7 @@
         btn.style.border = "none";
         btn.style.borderRadius = "8px";
         btn.style.fontSize = "12px";
-        btn.style.cursor = "pointer";
+        btn.style.cursor = "default";
 
         document.body.appendChild(btn);
 
