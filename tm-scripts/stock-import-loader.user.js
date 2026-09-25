d190016106065ca7aee1fe2dd07a447c9cb621c4
pandansu <dank15x@gmail.com>
2026-09-25 14:53:06 +0800

    Update stock-import-loader.user.js

--- tm-scripts/stock-import-loader.user.js
+++ tm-scripts/stock-import-loader.user.js
@@ -3,7 +3,7 @@
 // @namespace    stock-import-tool
 // @version      3.1
 // @description  Stock import tool
-// @match        https://scsm-djifx.lingyingdms.com/manage/receipt/purchasewarehouseorders*
+// @match        https://scsm-djifx.lingyingdms.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
 // @grant        none
 // @require      https://raw.githubusercontent.com/pandansu/eanloc/refs/heads/main/tm-scripts/stock-import-core.js?v=3.1
